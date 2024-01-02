0cda3a1c733df6fed12112a651c05930543c5a06
“Guada” <guadalupearroferia@gmail.com>
2024-01-02 20:49:43 +1000

    11 corrected cat position and shadow 12corrected circle postition created function disable btn

--- Assignment-12/JS/index.js
+++ Assignment-12/JS/index.js
@@ -1,50 +1,101 @@
 let tematica=document.getElementById("tematica");
 let selectValue=document.getElementsByTagName("option");
-let options=document.getElementById("colores");
+let chkSuperpuesto=document.getElementById("idChkSuperpuesto");
 let circles=document.getElementsByClassName("circle");
-
-tematica.innerText="Tematica elegida: Agencia de viajes";
+let idSelector=document.getElementById('idSelector');
+let windowWidth= window.innerWidth;
 
 documentColors=["#004165", "#fbc834","#00535e","#00a0af"];
 
-options.addEventListener('change', optionSelected);
+tematica.innerText="Tematica elegida: Agencia de viajes";
 
-// function optionSelect(){
-//     for (let a = 0; a <= selectValue.length; a++) {    
-//     let optionChosen=selectValue[a].value
-//         if (optionChosen && a !==0){
-//             let circleClass=circles[a].classList
-//             if (circleClass.contains("blue-circle")){
-//             circles[0].style.backgroundColor=documentColors[0];
-//             } else if (circleClass.contains("yellow-circle")){
-//                 circles[1].style.backgroundColor=documentColors[1];
-//             } else if (circleClass.contains("indigo-circle")){
-//                 circles[2].style.backgroundColor=documentColors[2];
-//             } else{
-//                 circles[3].style.backgroundColor=documentColors[3];
-//             }
-//         }
-//     }
-// }
+idSelector.innerHTML=crearSelect();
+
+function crearSelect(){
+    let contador=1;
+    let htmlSelect='<select name="colores" id="colores">';
+    htmlSelect= htmlSelect.concat('<option value="">Selecciona un color</option>');
+    for (let index = 0; index < documentColors.length; index++) {
+        htmlSelect= htmlSelect.concat('<option style="color:'+documentColors[index]+' " value="'+documentColors[index]+'" class="colorSamples"> Color N° '+contador+' </option>');
+        contador++;
+    }
+    htmlSelect= htmlSelect.concat('</select>');
+    return htmlSelect;
+}
+
+let options=document.getElementById("colores");
 
 
-// function optionSelected(){
-//     for (let v = 0; v < selectValue.length; v++) {
-//         selectValue[v]
-//         if (v!==0){
-//         if (selectValue[1]){
-//             circles[0].style.backgroundColor=documentColors[0];
-//         }else if (selectValue[2]){
-//             circles[1].style.backgroundColor=documentColors[1];
-     
-//         }
-//         else if (selectValue[3]){
-//             circles[2].style.backgroundColor=documentColors[2];
-    
-//         }
-//         else{
-//             circles[3].style.backgroundColor=documentColors[3];
-//         }
-//         }
-//     }
-// }
+function limpiarCirculos(){
+    for (let index = 0; index < circles.length; index++) {
+        circles[index].style.backgroundColor="white";     
+    }
+}
+
+function paintCircles(e){
+    limpiarCirculos();
+    let idSelected=e.target.getAttribute("id");
+    if(!chkSuperpuesto.checked)
+    {
+        let circuloSelected= document.getElementById(idSelected);
+        circuloSelected.style.backgroundColor=options[options.selectedIndex].value;
+    }
+    else{
+        for (let index = 0; index < circles.length; index++) {
+            if(circles[index].getAttribute("id") ==idSelected)
+            {
+                circles[index].style.backgroundColor=options[options.selectedIndex].value;
+                break;
+            }
+            else
+            {
+                circles[index].style.backgroundColor=options[options.selectedIndex].value;            
+            }           
+        }
+    }
+}
+
+addEventListenerCirculos();
+function addEventListenerCirculos(){
+    for (let index = 0; index < circles.length; index++) {
+        circles[index].addEventListener('click', paintCircles);
+    }
+}
+
+function removeEventListenerCirculos() {
+    for (let index = 0; index < circles.length; index++) {
+        circles[index].removeEventListener('click', paintCircles);
+    }
+}
+
+let colorSamples=document.getElementsByClassName("colorSamples");
+
+let buttonReset=document.getElementsByTagName("button");
+
+function enableBtnReset(){
+    buttonReset[0].addEventListener("click", limpiarCirculos);
+}
+
+function disableBtnReset(){
+    buttonReset[0].removeEventListener("click", limpiarCirculos);
+}
+function disableButtons(){
+    if (windowWidth <= 500){
+        for (let index = 0; index < circles.length; index++) {
+            circles[index].style.backgroundColor="#877f7d";     
+        }
+       options.disabled=true;
+       chkSuperpuesto.disabled=true;
+       removeEventListenerCirculos();
+       disableBtnReset();
+    } else {
+        options.disabled=!options.disabled;
+        chkSuperpuesto.disabled=!chkSuperpuesto.disabled;
+        enableBtnReset();
+    }
+}
+
+
+window.addEventListener("resize", disableButtons)
+
+disableButtons();
